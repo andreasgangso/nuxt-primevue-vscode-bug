2c9ebe83df3caffa7ca44a642ec844fbbf6ec3dc
Andreas Gangsø <andreasgangso@gmail.com>
2024-02-24 13:36:21 +0100

    initial

---
+++ components/Something.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const thing = ref("thing");
+</script>
+
+<template>
+  <div>Some {{ thing }}</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="w-screen h-screen flex items-center justify-center">
+    <h1>Hello</h1>
+    <Something />
+  </div>
+</template>
